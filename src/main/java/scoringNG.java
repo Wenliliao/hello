02b84174c73f55538961b576bd36ebc4cc984f07
Willis Liao <wenli0823@gmail.com>
2022-03-04 15:50:45 +0800

    scoringNg

---
+++ src/main/java/scoringNG.java
@@ -0,0 +1,12 @@
+public class scoringNG {
+    public static void main(String[] args) {
+        String[] names = {"tony","sam","wev","teb","rew"};
+        int[] math = {70,89,56,34,47};
+        int[] history = {60,56,76,84,45};
+        for (int i = 0; i<5; i++){
+            System.out.println(names[i] + "\t" + math[i] + "\t" + history[i] + "\t" + (math[i]+ history[i])/2);
+        }
+
+    }
+}
+
